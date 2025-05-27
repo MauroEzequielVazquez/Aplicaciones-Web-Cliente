4eae17dc385c38731d0df12057f42433ac1c0de6
Mauro <Mauroezequielvazquezgmail.com>
2025-05-27 19:25:18 -0300

    Act

--- scripts/impresoras.js
+++ scripts/impresoras.js
@@ -9,6 +9,7 @@ const products = [
     price: 340000,
     deliveryfree :true,
     oferta :true,
+    comprar: true // <- nuevo campo agregado
   },
   {
     title: "HP LaserJet S/F Pro M12w",
@@ -53,7 +54,7 @@ const products = [
     alt: "HP LaserJet Pro P1006",
     link: "../impresoras/impresora6.html",
     price: 110000,
-    deliveryfree :true,
+    deliveryfree :false,
     oferta :true,
     
   },
@@ -171,7 +172,7 @@ const products = [
     alt: "Brother M/F DCP-7055",
     link: "../impresoras/impresora19.html",
     price: 100000,
-    deliveryfree:false,
+    deliveryfree: true,
     oferta :false,
   },
   {
@@ -199,7 +200,7 @@ const products = [
     link: "../impresoras/impresora22.html",
         price: 100000,
           deliveryfree :false,
-    oferta :false,
+    oferta :true,
   },
   {
     title: "HP M/F Laserjet M201DW",
@@ -216,12 +217,12 @@ const products = [
     alt: "HP S/F Laserjet Pro P1606",
     link: "../impresoras/impresora24.html",
     price: 100000,
-    deliveryfree :true,
+    deliveryfree :false,
     oferta :true,
   },
  {
-    title: "Brother M/F DCP 8155DN",
-    img: "https://www.stec.com.ar/cdn/shop/files/29S0051-2_29772d74-61be-48f6-8352-b6efb458875c.jpg?v=1724092523",
+    title: "Lexmark S/F Ms431Dn",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqsHa65icXPnK8ttIIADcB0QPRUZ9VYgHVxw&s",
     alt: "Brother M/F DCP 8155DN",
     link: "../impresoras/impresora25.html",
     price: 250000,
@@ -236,8 +237,15 @@ const products = [
     price: 270000,
     deliveryfree :false,
     oferta :true,
-  }
-
+  } ,
+   {
+    title: "Hp S/F Laserjet 1020",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c02920588.png",
+    link: "../impresoras/impresora27.html",
+    price: 85000,
+    deliveryfree: true,
+    oferta: false,
+  },
 
 
 
@@ -245,10 +253,6 @@ const products = [
 ];
 
 const container = document.querySelector('.listadoImpresoras'); // listadoimpresoras es la class del section que contenía el html de las card, o sea la class de la etiqueta PADRE!!
-                                                        
-// función para CREAR TARJETAS de impresoras
-// PRIMERO creamos la card que es un article, y dentro del article metemos las card
-
 
 // función para CREAR TARJETAS de impresoras
 // PRIMERO creamos la card que es un article, y dentro del article metemos las card
@@ -265,6 +269,7 @@ function createProductCard(product) {
   img.alt = product.alt;
 
   const price = document.createElement('p');
+  price.classList.add('price') /*para manipularla en css*/
   price.textContent = 'Precio: $' + product.price;
 
   const link = document.createElement('a');
@@ -272,7 +277,6 @@ function createProductCard(product) {
   link.classList.add('boton-vermas');
   link.textContent = 'Ver más';
 
- 
   const oferta = document.createElement('p');
   if (product.oferta === true) {
     oferta.textContent = '¡Oferta disponible!';
@@ -286,28 +290,32 @@ function createProductCard(product) {
     envio.style.color = 'green';
   }
 
+  // botón comprar (lo mostramos siempre, porque lo vas a usar para un futuro carrito)
+  const botonComprar = document.createElement('button');
+  botonComprar.textContent = 'Comprar';
+  botonComprar.classList.add('boton-comprar');
+  // podés agregar un eventListener en el futuro para que agregue al carrito
+
   // ahora ya tenemos los elementos creados pero SUELTOS, entonces vamos a agregarlos a la variable card.appendChild y vamos a agregar a los hijos
   // card es la VARIABLE article, donde contengo toda la info de los prod
-
   card.appendChild(title);
   card.appendChild(img);
- // card.appendChild(price);  // no quiero mostrar el precio en la pantalla principal, que en las impresoras puedan aceder a detalles
+  card.appendChild(price);  // no quiero mostrar el precio en la pantalla principal, que en las impresoras puedan aceder a detalles
   card.appendChild(oferta); // solo se agrega si es true
-  card.appendChild(envio);  // solo se agrega si es true
+  card.appendChild(envio);  
   card.appendChild(link);
+  card.appendChild(botonComprar); // lo agregamos al final
 
   return card; // puede tener o no return, y no tenés que especificar qué tipo de dato devuelve en JS, tenés que prestar atención
                // ya retorno la variable card con todos los elementos hijos cargados
 }
 
-
 // luego hacemos un forEach y recorremos la variable products, para que vaya creando las tarjetas, por cada prod crea 1 tarjeta
 // en cada vuelta le va asignando a la variable product el elemento de la tarjeta
 products.forEach(product => {
   const card = createProductCard(product);                       // le paso de parámetro el producto que recorro
-  container.appendChild(card);                                               // luego debo INSERTARLO A MI DOM
+  container.appendChild(card);                                   // luego debo INSERTARLO A MI DOM
 });
-                                                 
 
 
 
@@ -321,9 +329,7 @@ products.forEach(product => {
 
 const button = document.querySelector('#btn-add-products');
 
-button.addEventListener('click', () => {
-  alert('Agregado!');
-});
+
 
 
 
@@ -333,6 +339,8 @@ const inputNombre = document.querySelector('#busqueda');
 const inputPrecioMax = document.querySelector('#precio-max');
 const inputPrecioMin = document.querySelector('#precio-min');
 inputNombre.addEventListener("input", aplicarFiltros);
+inputPrecioMin.addEventListener("input", aplicarFiltros);
+inputPrecioMax.addEventListener("input", aplicarFiltros);
 
 
 
@@ -375,7 +383,49 @@ function aplicarFiltros() {
 
   document.getElementById("ofertas").addEventListener("change", aplicarFiltros);
   document.getElementById("envio").addEventListener("change", aplicarFiltros);
-  renderPrinters(impresoras);
+
+
+
+//func para limpiar  filtros
+
+const btnLimpiar = document.querySelector('#limpiar-filtros');
+btnLimpiar.addEventListener('click', () => {
+  inputNombre.value = '';
+  inputPrecioMin.value = '';
+  inputPrecioMax.value = '';
+  document.getElementById("ofertas").checked = false;
+  document.getElementById("envio").checked = false;
+  aplicarFiltros(); // vuelve a mostrar todo
+});
+
+
+
+// func add
+
+if (product.oferta === true) {
+  oferta.textContent = 'OFERTA';
+  oferta.classList.add('etiqueta', 'oferta');
+}
+
+if (product.deliveryfree === true) {
+  envio.textContent = 'ENVÍO GRATIS';
+  envio.classList.add('etiqueta', 'envio');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -407,3 +457,88 @@ console.log('suma', sum);
 // const filteredProducts = products.filter(p => p.name.includes('a'));
 // console.log('producto encontrado', foundProduct);
 // console.log('productos filtrados', filteredProducts);
+
+
+
+
+
+// //clase 7  : probamos el llamado de prod mediante una Appi
+
+
+//Código completo: el inicio es igual que como hicimos para hardcodear la lista de prod
+
+// const grid = document.querySelector('.listadoImpresoras');
+// const searchInput = document.querySelector('#input-search-products');
+// const deliveryFreeCheckBox = document.querySelector('#delivery-free');
+
+// function createProductCard(product) {
+//     const card = document.createElement('article');
+//     card.classList.add('product-card');
+
+//     const img = document.createElement('img');
+//     img.src = product.thumbnail;
+//     img.alt = product.title;
+
+//     const title = document.createElement('h3');
+//     title.textContent = product.title;
+
+//     const description = document.createElement('p');
+//     description.textContent = product.description;
+
+//     const price = document.createElement('p');
+//     price.textContent = `$${product.price}`;
+
+//     const button = document.createElement('button');
+//     button.textContent = 'Comprar';
+
+//     card.appendChild(img);
+//     card.appendChild(title);
+//     card.appendChild(description);
+//     card.appendChild(price);
+//     card.appendChild(button);
+
+//     return card;
+// }
+
+
+
+// function renderProducts(list) {
+//     list.forEach(product => {
+//         const card = createProductCard(product);
+//         grid.appendChild(card);
+//     });
+// }
+
+
+
+//Luego hacemos una funcion async
+
+// const getProducts = async () => {
+//     const response = await fetch('https://dummyjson.com/products', {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+
+//     const data = await response.json();
+//     console.log('data', data);
+//     renderProducts(data.products);
+// }
+
+// // Clase 7: probamos el llamado de prod mediante una API
+// getProducts();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/toner.js
+++ scripts/toner.js
@@ -2,84 +2,84 @@
 tarjetas de toner*/
 const toners = [
   {
-    title: "Tóner Alternativo 85A",
+    title: "Tóner Alternativo Hp 85A",
     img: "https://http2.mlstatic.com/D_NQ_NP_958637-MLA41535934965_042020-O.webp",
     alt: "Tóner 85A para HP",
     link: "#",
     price: 9500
   },
   {
-    title: "Tóner Alternativo 79A",
+    title: "Tóner Alternativo Hp 79A",
     img: "https://http2.mlstatic.com/D_NQ_NP_973244-MLA77095171285_062024-O.webp",
     alt: "Tóner 79A para HP",
     link: "#",
     price: 9000
   },
   {
-    title: "Tóner Alternativo TN750",
+    title: "Tóner Alternativo Brother TN750",
     img: "https://http2.mlstatic.com/D_668323-MLA32407032384_102019-O.jpg",
     alt: "Tóner TN750 para Brother",
     link: "#",
     price: 16000
   },
   {
-    title: "Tóner Alternativo TN450",
+    title: "Tóner Alternativo Brother TN450",
     img: "https://diamondsystemar.vtexassets.com/arquivos/ids/160293/4505129450_1.jpg?v=638544099229930000",
     alt: "Tóner TN450 para Brother",
     link: "#",
     price: 14500
   },
   {
-    title: "Tóner Alternativo TN660",
+    title: "Tóner Alternativo  Brother TN660",
     img: "https://http2.mlstatic.com/D_NQ_NP_668107-MLA84477130829_052025-O.webp",
     alt: "Tóner TN660 para Brother",
     link: "#",
     price: 11000
   },
   {
-    title: "Tóner Alternativo CF283A",
+    title: "Tóner Alternativo Hp CF283A",
     img: "https://http2.mlstatic.com/D_NQ_NP_973244-MLA77095171285_062024-O.webp",
     alt: "Tóner 83A para HP",
     link: "#",
     price: 9000
   },
   {
-    title: "Tóner Alternativo CF226A",
+    title: "Tóner Alternativo Hp CF226A",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgGMWQ0arfGs0uoALLnFv2kV1E1zCfJfKC8A&s",
     alt: "Tóner 26A para HP",
     link: "#",
     price: 13500
   },
   {
-    title: "Tóner Alternativo 78A",
+    title: "Tóner Alternativo Hp 78A",
     img: "https://www.powerprinter.com.ar/web2023/801-home_default/toner-hp-78a-ce278a-negro-alternativo-gneiss.jpg",
     alt: "Tóner 78A para HP",
     link: "#",
     price: 9200
   },
   {
-    title: "Tóner Alternativo 12A",
+    title: "Tóner Alternativo Hp 12A",
     img: "https://www.soscomputacion.com.ar/26469/toner-alternativo-evertec-hp-12a-negro-hp-laserjet-1010-1012-1015-1018-1020-1022-1005-3015-3020-3030.jpg",
     alt: "Tóner 12A para HP",
     link: "#",
     price: 9000
   },
   {
-    title: "Tóner Alternativo TN580",
-    img: "https://diamondsystemar.vtexassets.com/arquivos/ids/160295-800-450?v=638544115572100000&width=800&height=450&aspect=true",
+    title: "Tóner Alternativo  Brother TN580",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnXAq9CcDz7lcQhLXjQDWQq6VWFhE8paQ1uw&s",
     alt: "Tóner TN580 para Brother",
     link: "#",
     price: 16000
   },
   {
-    title: "Tóner Alternativo TN880",
+    title: "Tóner Alternativo  Brother TN880",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQMHeLmiwPgA_GGhwNvlp7oz4R3-EqnOlLEQ&s",
     alt: "Tóner TN880 para Brother",
     link: "#",
     price: 16000
   },
   {
-    title: "Tóner Alternativo TN1060",
+    title: "Tóner Alternativo  Brother TN1060",
     img: "https://axa.com.ar/webaxa/2995-home_default/toner-para-brother-tn-1060-alternativo.jpg",
     alt: "Tóner TN1060 para Brother",
     link: "#",
@@ -100,56 +100,56 @@ const toners = [
     price: 15000
   },
   {
-    title: "Drum Alternativo tn1060",
+    title: "Drum Alternativo Brother tn1060",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbaFW4gJsIN8JLruMJNqUqDtB3Q-tdAkaukA&s",
     alt: "Drum Alternativo tn1060",
     link: "#",
     price: 9500
   },
   {
-    title: "Drum Alternativo tn410",
+    title: "Drum Alternativo Brother tn410",
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_757214-MLU78335534840_082024-T.webp",
     alt: "Drum Alternativo tn410",
     link: "#",
     price: 8700
   },
   {
-    title: "Drum Alternativo tn580",
+    title: "Drum Alternativo Brother tn580",
     img: "https://www.soscomputacion.com.ar/13798/toner-alternativo-para-brother-tn-600-2370-660-2320-2360-2540.jpg",
     alt: "Drum Alternativo tn580",
     link: "#",
     price: 13800
   },
   {
-    title: "Tóner Alternativo TN360",
+    title: "Tóner Alternativo Brother TN360",
     img: "https://bainformatica.com.ar/wp-content/uploads/2021/06/toner-alternativo-brother-tn360-1.jpg",
     alt: "Tóner Alternativo TN360",
     link: "#",
     price: 9700
   },
   {
-    title: "Tóner Alternativo CF210X",
+    title: "Tóner Alternativo Hp CF210X",
     img: "https://http2.mlstatic.com/D_NQ_NP_751035-MLA54399323328_032023-O.webp",
     alt: "Tóner 210X para HP",
     link: "#",
     price: 12500
   },
   {
-    title: "Tóner Alternativo 35A",
+    title: "Tóner Alternativo Hp 35A",
     img: "https://http2.mlstatic.com/D_NQ_NP_712149-MLA70399423949_072023-O.webp",
     alt: "Tóner 35A para HP",
     link: "#",
     price: 9100
   },
    {
-    title: "Tóner Alternativo 35A",
+    title: "Tóner Alternativo Hp 35A",
     img: "https://http2.mlstatic.com/D_NQ_NP_712149-MLA70399423949_072023-O.webp",
     alt: "Tóner 35A para HP",
     link: "#",
     price: 9100
   },
    {
-    title: "Tóner Alternativo 35A",
+    title: "Tóner Alternativo Hp 35A",
     img: "https://http2.mlstatic.com/D_NQ_NP_712149-MLA70399423949_072023-O.webp",
     alt: "Tóner 35A para HP",
     link: "#",
@@ -157,7 +157,7 @@ const toners = [
   }
   ,
    {
-    title: "Tóner Alternativo 35A",
+    title: "Tóner Alternativo Hp 55A",
     img: "https://http2.mlstatic.com/D_NQ_NP_712149-MLA70399423949_072023-O.webp",
     alt: "Tóner 35A para HP",
     link: "#",
@@ -165,7 +165,7 @@ const toners = [
   }
   ,
    {
-    title: "Tóner Alternativo 35A",
+    title: "Tóner Alternativo Hp 35A",
     img: "https://http2.mlstatic.com/D_NQ_NP_712149-MLA70399423949_072023-O.webp",
     alt: "Tóner 35A para HP",
     link: "#",
@@ -215,3 +215,56 @@ toners.forEach(toner => {
   const card = createTonerCard(toner);
   listado.appendChild(card);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//filtros
+
+
+const inputNombre = document.querySelector('#busqueda');
+const inputPrecioMax = document.querySelector('#precio-max');
+const inputPrecioMin = document.querySelector('#precio-min');
+inputNombre.addEventListener("input", aplicarFiltros);
+
+
+
+
+
+
+// función para mostrar solo los productos filtrados
+
+function aplicarFiltros() {
+  const texto = inputNombre.value.toLowerCase();
+  const precioMin = parseInt(inputPrecioMin.value) || 0;
+  const precioMax = parseInt(inputPrecioMax.value) || Infinity;
+
+  listado.innerHTML = '';
+
+  const productosFiltrados = toners.filter(toner => {
+    const coincideNombre = toner.title.toLowerCase().includes(texto);
+    const coincidePrecio = toner.price >= precioMin && toner.price <= precioMax;
+
+    return coincideNombre && coincidePrecio;
+  });
+
+  if (productosFiltrados.length === 0) {
+    listado.innerHTML = '<p>No se encontraron productos, por favor vuelva a intentar.</p>';
+    return;
+  }
+
+  productosFiltrados.forEach(toner => {
+    const card = createTonerCard(toner);
+    listado.appendChild(card);
+  });
+}
